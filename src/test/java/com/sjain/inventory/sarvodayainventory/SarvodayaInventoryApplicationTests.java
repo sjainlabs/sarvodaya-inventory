d7054fbce311c260ce37a3fc87891943dfbadba4
Jain, Sapan <C-Sapan.Jain@charter.com>
2022-11-09 04:05:12 -0600

    initial commit

---
+++ src/test/java/com/sjain/inventory/sarvodayainventory/SarvodayaInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjain.inventory.sarvodayainventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarvodayaInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
